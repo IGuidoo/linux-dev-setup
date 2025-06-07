f48b41d78200724d352795dac6a6830bf964e7a9
IGuidoo <iguidoo@outlook.com>
2025-06-07 20:06:39 +0200

    Add PNPM_HOME environment variable setup to shell profiles

--- setup-dev-environment.sh
+++ setup-dev-environment.sh
@@ -61,13 +61,18 @@ if [ -f ~/.bashrc ]; then
     echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
     echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
     echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.bashrc
+    echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.bashrc
+    echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.bashrc
 fi
 
 if [ -f ~/.zshrc ]; then
     echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
     echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
     echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.zshrc
+    echo 'export PNPM_HOME="$HOME/.local/share/pnpm"' >> ~/.zshrc
+    echo 'export PATH="$PNPM_HOME:$PATH"' >> ~/.zshrc
 fi
+
 
 # Load aliases
 source ./configs/aliases.sh
